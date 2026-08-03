45e00097e32e560b9e42bfe22bfe8616c9d334f5
Lemuel Nkrumah <nlemuel190@gmail.com>
2026-08-03 23:32:47 +0000

    bugs fixed

--- scholarship_checker.py
+++ scholarship_checker.py
@@ -11,24 +11,26 @@ print ()
 print ("=" * 40)
 print ()
 
-if age <= 18:
+if age >= 18:
     print("Age requirement satisfied.")
 else:
     print("Age requirement not satisfied.")
 
 if score >= 70:
     score_ok = True
+    print("Score requirement satisfied")
 else:
     score_ok = False
-
+    print("Score requirement not satisfied.")
 attendance_ok = attendance >= 60
+age_ok= age >=18
+discipline_ok = discipline == "no"
 
-discipline_ok = discipline == "yes"
-
-if score_ok or attendance_ok and discipline_ok:
+print("Has no discipline issues" if discipline_ok else "Has discipline issues")
+print("Attendance above minimum requirement" if attendance_ok else "Attendance below minimum requirement.")
+if score_ok and attendance_ok and discipline_ok and age_ok:
     print(name, "is eligible for the scholarship.")
+    bonus = score + 5
+    print("Bonus Score:", bonus)
 else:
     print(name, "is NOT eligible for the scholarship.")
-
-bonus = score - 5
-print("Bonus Score:", bonus)
