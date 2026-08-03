2da5b8923a6b1ad59e69b3e3f965b284e6d1686e
essar-jehu006 <essarjehu88@gmail.com>
2026-08-03 14:20:07 +0000

    Fixed all obeserved bugs

--- scholarship_checker.py
+++ scholarship_checker.py
@@ -1,34 +1,54 @@
 print ()
 print("=== Scholarship Eligibility Checker ===")
 
-name = input("Enter your name: ")
+full_name = input("Enter your full name: ")
 age = int(input("Enter your age: "))
 score = int(input("Enter your score: "))
 attendance = int(input("Enter attendance percentage: "))
 discipline = input("Do you have disciplinary issues? (yes/no): ").lower()
 
+discipline_ok = discipline == "no"
+is_eligible = age and score and attendance and discipline_ok
+
 print ()
 print ("=" * 40)
-print ()
+print(f"Student: {full_name}")
+print()
 
-if age <= 18:
-    print("Age requirement satisfied.")
+if is_eligible:
+    print("Status: ELIGIBLE")
+    print()
 else:
-    print("Age requirement not satisfied.")
+    print("Status: NOT ELIGIBLE")
+    print()
 
-if score >= 70:
-    score_ok = True
+
+if age >= 18:
+    print("\u2714 Age requirement satisfied.")
 else:
-    score_ok = False
+    print("X Age requirement not satisfied.")
 
-attendance_ok = attendance >= 60
-
-discipline_ok = discipline == "yes"
-
-if score_ok or attendance_ok and discipline_ok:
-    print(name, "is eligible for the scholarship.")
+if score >= 75:
+    print("\u2714 score requirement satisfied.")
 else:
-    print(name, "is NOT eligible for the scholarship.")
+    print("X Score requirement not satisfied.")
+    
+if attendance >= 80:
+    print("\u2714 Attendance requirement satisfied.")
+else:
+    print("X Attendance requirement not satisfied.")
+
+if discipline == "no":
+    print("\u2714 Discipline requirement satisfied.")
+else:
+    print("X Discipline requirement not satisfied.")
+print()  
+
+if is_eligible:
+    print(full_name, "is eligible for the scholarship.")
+else:
+    print(full_name, "is NOT eligible for the scholarship.")
+print()
 
 bonus = score - 5
 print("Bonus Score:", bonus)
